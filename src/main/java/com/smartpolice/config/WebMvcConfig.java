5d1601ff4c0cbfe9aab8ef913b1430a63967ce97
inditechIOT <inditechelectrosystems@gmail.com>
2025-06-03 15:47:04 +0530

    Initial commit - Spring Boot project

--- src/main/java/com/smartpolice/config/WebMvcConfig.java
+++ src/main/java/com/smartpolice/config/WebMvcConfig.java
@@ -1,26 +1,24 @@
 package com.smartpolice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer{
-	
-	 @Override
-	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	        registry.addResourceHandler("/**")
-	                .addResourceLocations("classpath:/static/");
-	    }
-	
-	
-	 @Override
-     public void addCorsMappings(CorsRegistry registry) {
-         registry.addMapping("/**")  // Applies to all endpoints
-                 .allowedOrigins("http://172.105.47.237")  // Your React/Angular app URL
-                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                 .allowedHeaders("*")
-                 .allowCredentials(true);  // Optional, for cookies/auth headers
-     }
+public class WebMvcConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://172.105.61.131:8081")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
